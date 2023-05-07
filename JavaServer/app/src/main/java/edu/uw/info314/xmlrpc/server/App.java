f1311867a4969f2f56ac55675e56567dd1bc5d1d
jellymawan <62910852+jellymawan@users.noreply.github.com>
2023-05-06 17:20:32 -0700

    Not complete

--- JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
+++ JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
@@ -2,7 +2,7 @@ package edu.uw.info314.xmlrpc.server;
 
 import java.util.*;
 import java.util.logging.*;
-
+import java.util.ArrayList; // import the ArrayList class
 import static spark.Spark.*;
 
 class Call {
@@ -14,15 +14,54 @@ public class App {
     public static final Logger LOG = Logger.getLogger(App.class.getCanonicalName());
 
     public static void main(String[] args) {
-        LOG.info("Starting up on port 4567");
+        LOG.info("Starting up on port 8080");
+        port(8080); //sets port to 8080
 
         // This is the mapping for POST requests to "/RPC";
         // this is where you will want to handle incoming XML-RPC requests
-        post("/RPC", (request, response) -> { response.status(500); return "TBD"; });
+        post("/", (request, response) -> { //will need to change this to /RPC
+            response.status(200); 
+            ArrayList<Integer> nums = new ArrayList<Integer>(); // Create an ArrayList object
+            String requestBody = request.body();
+            String methodName = requestBody.substring(requestBody.indexOf("<methodName>") + 12, requestBody.indexOf("<params>"));
+            int index = requestBody.indexOf("<id>"); //seems like the number is always next to a <id> tag
+            int number = 0;
 
+            //PARSES BODY
+            while (index != -1) {  //gets each instance of <id> tag
+                requestBody = requestBody.substring(index + 1);
+                number = Integer.parseInt(requestBody.substring(3, requestBody.indexOf("</i4>")));
+                nums.add(number); //adds each number to be added to the arraylist
+                index = requestBody.indexOf("<id>");
+            }
+
+            LOG.info(methodName);
+            LOG.info(nums + "");
+
+            int[] arr = nums.stream().mapToInt(i -> i).toArray();
+
+            //CREATE INSTANCE OF CALCULATOR CLASS
+            Calc calculator = new Calc();
+
+            //CALLS CORRECT METHOD
+            if (methodName.equals("add")){
+                return calculator.add(arr);
+            } else if(methodName.equals("subtract")) {
+                return calculator.subtract(arr[0], arr[1]);
+            } else if(methodName.equals("multiply")) {
+                LOG.info(calculator.multiply(arr) + ""); //returns 0
+                int temp = arr[0] * arr[1];
+                LOG.info(temp + ""); //returns 12
+                return calculator.multiply(arr); //returns 0
+            } else if(methodName.equals("divide")){
+                return calculator.divide(arr[0], arr[1]);
+            } else{
+                return calculator.modulo(arr[0], arr[1]);
+            }
         // Each of the verbs has a similar format: get() for GET,
         // put() for PUT, delete() for DELETE. There's also an exception()
         // for dealing with exceptions thrown from handlers.
         // All of this is documented on the SparkJava website (https://sparkjava.com/).
+        });
     }
 }

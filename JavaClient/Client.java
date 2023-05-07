5f38bafdc3cda380cadfad1a2da6066fa118a25a
jellymawan <62910852+jellymawan@users.noreply.github.com>
2023-05-06 17:18:09 -0700

    Gives warning message, multiply not correct

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -13,8 +13,13 @@ import org.w3c.dom.Node;
  */
 public class Client {
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    static String host = "";
+    static int port = 0;
 
     public static void main(String... args) throws Exception {
+        host = args[0];
+        port = Integer.valueOf(args[1]);
+
         System.out.println(add() == 0);
         System.out.println(add(1, 2, 3, 4, 5) == 15);
         System.out.println(add(2, 4) == 6);
@@ -25,24 +30,50 @@ public class Client {
         System.out.println(modulo(10, 5) == 0);
     }
     public static int add(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendRequest("add", lhs, rhs);
     }
     public static int add(Integer... params) throws Exception {
-        return -1;
+        return sendRequest("add", params);
     }
     public static int subtract(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendRequest("subtract",  lhs, rhs);
     }
     public static int multiply(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendRequest("multiply", lhs, rhs);
     }
     public static int multiply(Integer... params) throws Exception {
-        return -1;
+        return sendRequest("multiply", params);
     }
     public static int divide(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendRequest("divide",  lhs, rhs);
     }
     public static int modulo(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendRequest("modulo",  lhs, rhs);
+    }
+
+
+    public static int sendRequest(String methodName, Object... arguments) throws Exception{
+        //Create instance of client
+        HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
+
+        //Create request body
+        String parameters = "<params>";
+        for (Object param: arguments) {
+            parameters += "<param><value><id>" + param + "</i4></value></param>";
+        }
+        parameters += "</params>";
+
+        String requestBody = "<?xml version='1.-'?><methodCall><methodName>" + methodName + parameters + "</methodName></methodCall>";
+
+        //send request
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("http://" + host + ":" + port))
+        .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+        .header("Content-Type", "text/xml")
+        .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return Integer.parseInt(response.body());
     }
 }
